ce1243ee716ce938ce5b9c706659ca7bb3d5f978
tangyu <13438422109@163.com>
2022-07-25 21:55:11 +0800

    first commit

---
+++ src/main/java/com/yu/HelloTest.java
@@ -0,0 +1,12 @@
+package com.yu;
+
+/**
+ * @Author Yu Ge
+ * @Date 2022/7/25 21:53
+ * @Version 1.0
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
