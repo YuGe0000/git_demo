7431bdf936114a813b070422ee9b7eb7a522bda8
tangyu <13438422109@163.com>
2022-07-25 22:09:10 +0800

    hot-fix merge commit

--- src/main/java/com/yu/HelloTest.java
+++ src/main/java/com/yu/HelloTest.java
@@ -11,5 +11,6 @@ public class HelloTest {
         System.out.println("hello git 2");
         System.out.println("hello git 3");
         System.out.println("hello git 4 hot-fix");
+        System.out.println("hot-fix merge test");
     }
 }
